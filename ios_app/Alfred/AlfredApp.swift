c425ac17d96bb1392fd181593a1ee795e4c5d3fa
norika <norika1207@gmail.com>
2026-04-26 19:33:29 +0800

    auto: update AlfredApp.swift

--- ios_app/Alfred/AlfredApp.swift
+++ ios_app/Alfred/AlfredApp.swift
@@ -2,7 +2,6 @@ import SwiftUI
 
 @main
 struct AlfredApp: App {
-    @StateObject private var auth = AuthManager.shared
     @StateObject private var locationManager = LocationManager.shared
     @StateObject private var backgroundManager = BackgroundManager.shared
     @StateObject private var healthKit = HealthKitManager.shared
@@ -10,29 +9,16 @@ struct AlfredApp: App {
 
     var body: some Scene {
         WindowGroup {
-            Group {
-                if auth.isLoggedIn {
-                    AlfredView()
-                        .onAppear {
-                            backgroundManager.start()
-                            Task { await healthKit.requestPermissions() }
-                        }
-                        .onDisappear {
-                            backgroundManager.stop()
-                        }
-                } else {
-                    LoginView()
-                        .onAppear {
-                            backgroundManager.stop()
-                        }
+            AlfredView()
+                .onAppear {
+                    backgroundManager.start()
+                    Task { await healthKit.requestPermissions() }
                 }
-            }
-            .preferredColorScheme(.dark)
-            .environmentObject(auth)
+                .preferredColorScheme(.dark)
         }
         .onChange(of: scenePhase) { _, phase in
             BackgroundManager.shared.isAppActive = (phase == .active)
-            if phase == .active && auth.isLoggedIn {
+            if phase == .active {
                 Task { await LocationManager.shared.checkContext() }
             }
         }
